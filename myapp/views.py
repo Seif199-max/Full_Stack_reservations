a09d8e03ea71fc73fbb66cede86d7e20b3599052
Seif199-max <elhosseny199@gmail.com>
2025-11-08 16:19:40 +0200

    Final Version

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Menu
 from django.core import serializers
@@ -26,9 +27,9 @@ def book(request):
 
 #Add code for the bookings() view
 
-def bookings(request):
+def reservations(request):
     data = request.GET.get('date',datetime.today().date())
-    bookings = Booking.objects.all()
+    bookings = Booking.objects.filter(reservation_date=data)
     booking_json = serializers.serialize('json', bookings)
     return render(request, 'bookings.html', {"bookings": booking_json})
 
@@ -43,4 +44,49 @@ def display_menu_item(request, pk=None):
         menu_item = Menu.objects.get(pk=pk) 
     else: 
         menu_item = "" 
-    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
+    return render(request, 'menu_item.html', {"menu_item": menu_item})
+
+@csrf_exempt
+def bookings(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        exits = Booking.objects.filter(reservation_date=data['reservation_date']).filter(reservation_slot=data['reservation_slot']).exists()
+        if not exits:
+            booking =Booking(
+                first_name=data['first_name'],
+                reservation_date=data['reservation_date'],
+                reservation_slot=data['reservation_slot'],)
+            booking.save()
+        else:
+            return HttpResponse(
+                json.dumps({'error': 1}),
+                content_type='application/json'
+            )
+
+    date = request.GET.get('date',datetime.today().date())
+    bookings = Booking.objects.all().filter(reservation_date=date)
+    booking_json = serializers.serialize('json', bookings)
+    return HttpResponse(booking_json, content_type='application/json')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
